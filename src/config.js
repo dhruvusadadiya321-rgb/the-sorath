c926fc2bf3d6f5f1c7d63aec01853b0fc05602a8
dhruv <dhruvusadadiya321@gmail.com>
2025-12-24 17:19:59 +0530

    Connected frontend to live backend

--- src/config.js
+++ src/config.js
@@ -6,6 +6,6 @@ const isLocal = window.location.hostname === "localhost";
 // Jo Local hoy to localhost:5000 vapar, nahitar Render vali link vapar
 export const API_URL = isLocal 
   ? "http://localhost:5000/api" 
-  : "https://the-sorath-backend.onrender.com/api";
+  : "https://the-sorath-backend.onrender.com";
 
 // Aa file tamne automatic sachi link apse!
